92f94c8a851392fd20d1d8d3554a691b439e97ca
ruanha <rune_hartvig@hotmail.com>
2018-05-26 23:17:32 +0200

    finished all excercises

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,15 @@
 class Timer
-  #write your code here
+    def initialize
+        @seconds = 0
+    end
+
+    attr_accessor :seconds
+
+    def time_string
+        puts @seconds
+        hrs = @seconds/3600
+        min = (@seconds-hrs*3600)/60
+        sec = @seconds-hrs*3600-min*60
+        sprintf("%02d:%02d:%02d", hrs, min, sec)
+    end
 end
