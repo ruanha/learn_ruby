e952098445fd5bd6a21ccb4504fc2ea0a39e97a2
ruanha <rune_hartvig@hotmail.com>
2018-05-26 20:40:49 +0200

    added solution for book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
 class Book
-# write your code here
+    attr_reader :title
+
+    #writer method
+    def title=(str)
+        exceptions=["a", "the", "and", "in", "of", "an", "in"]
+
+        array = str.split(" ")
+        array.each do |a|
+            if !exceptions.include? a
+                a.capitalize!
+            end
+        end
+        array.first.capitalize!
+        @title = array.join(" ")
+    end
+
 end
